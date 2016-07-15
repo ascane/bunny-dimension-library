f6681a9da9728338390e2c36db20389f023d251f
SuperContact <supercontact@126.com>
2016-07-15 15:31:12 +0100

    Add AbstractMultiset and simplify HashMultiset

---
+++ src/bunny/structure/AbstractMultiset.java
@@ -0,0 +1,173 @@
+package bunny.structure;
+
+import java.util.AbstractSet;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.function.Function;
+
+import bunny.wrap.SetWrapper;
+
+// You need to implement: size(), count(), setCount(), elementSet(), and clear() for performance.
+public abstract class AbstractMultiset<T> extends AbstractSet<T> implements Multiset<T> {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean contains(Object element) {
+		return count((T)element) > 0;
+	}
+	
+	@Override
+	public boolean add(T element) {
+		int old = count(element);
+		setCount(element, old + 1);
+		return true;
+	}
+	
+	@Override
+	public int add(T element, int occurrences) {
+		if (occurrences < 0) throw new IllegalArgumentException("Occurrences cannot be negative!");
+		int old = count(element);
+		setCount(element, old + occurrences);
+		return old;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean remove(Object element) {
+		int old = count((T)element);
+		if (old > 0) {
+			setCount((T)element, old - 1);
+			return true;
+		}
+		return false;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public int remove(Object element, int occurrences) {
+		if (occurrences < 0) throw new IllegalArgumentException("Occurrences cannot be negative!");
+		int old = count((T)element);
+		if (old > 0) {
+			setCount((T)element, Math.max(old - occurrences, 0));
+		}
+		return old;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		if (c == null) return false;
+		boolean changed = false;
+		for (Object o : c) {
+			if (setCount((T)o, 0) > 0) {
+				changed = true;
+			}
+		}
+		return changed;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		if (c == null) return false;
+		boolean changed = false;
+		HashSet<T> cache = new HashSet<T>();
+		for (Object o : c) {
+			int count = count((T)o);
+			if (count > 0) {
+				cache.add((T)o);
+			}
+		}
+		for (Iterator<Entry<T>> it = entrySet().iterator(); it.hasNext();) {
+			Entry<T> entry = it.next();
+			if (!cache.contains(entry.getElement())) {
+				it.remove();
+				changed = true;
+			}
+		}
+		return changed;
+	}
+	
+	@Override
+	public void clear() {
+		for (T element : elementSet()) {
+			setCount(element, 0);
+		}
+	}
+
+	@Override
+	public Set<Entry<T>> entrySet() {
+		return new SetWrapper<T, Entry<T>>(elementSet(), new Function<T, Entry<T>>() {
+			@Override
+			public Entry<T> apply(T t) {return new MultisetEntry(t);}
+		});
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return new MultisetIterator();
+	}
+
+	private class MultisetEntry implements Entry<T> {
+		
+		private T element;
+		private int count;
+		
+		public MultisetEntry(T element) {
+			this.element = element;
+			count = count(element);
+		}
+		
+		@Override
+		public T getElement() {return element;}
+		@Override
+		public int getCount() {return count;}
+		
+	}
+	
+	private class MultisetIterator implements Iterator<T> {
+
+		private Iterator<Entry<T>> entryIterator;
+		private T current;
+		private T last;
+		private int currentCount = 0;
+		private int i = 0;
+		
+		public MultisetIterator() {
+			entryIterator = entrySet().iterator();
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return i < currentCount || entryIterator.hasNext();
+		}
+
+		@Override
+		public T next() {
+			if (i < currentCount) {
+				i++;
+				return current;
+			}
+			if (entryIterator.hasNext()) {
+				Entry<T> entry = entryIterator.next();
+				current = entry.getElement();
+				last = current;
+				currentCount = entry.getCount();
+				i = 1;
+				return current;
+			}
+			throw new NoSuchElementException();
+		}
+		
+		@Override
+		public void remove() {
+			if (last == null) throw new IllegalStateException();
+			AbstractMultiset.this.remove(last);
+			last = null;
+		}
+	}
+	
+}
--- src/bunny/structure/HashMultiset.java
+++ src/bunny/structure/HashMultiset.java
@@ -1,18 +1,12 @@
 package bunny.structure;
 
-import java.util.AbstractSet;
 import java.util.Collection;
-import java.util.Map;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
 import java.util.Set;
-import java.util.function.Function;
 
 import bunny.wrap.SetView;
-import bunny.wrap.SetWrapper;
 
-public class HashMultiset<T> extends AbstractSet<T> implements Multiset<T>{
+public class HashMultiset<T> extends AbstractMultiset<T> {
 
 	private int count = 0;
 	private HashMap<T, Integer> map;
@@ -29,10 +23,12 @@ public class HashMultiset<T> extends AbstractSet<T> implements Multiset<T>{
 	public int size() {
 		return count;
 	}
+	
 	@Override
 	public int count(T element) {
 		return map.getOrDefault(element, 0);
 	}
+	
 	@Override
 	public int setCount(T element, int count) {
 		if (count < 0) throw new IllegalArgumentException("Element count cannot be negative!");
@@ -45,181 +41,16 @@ public class HashMultiset<T> extends AbstractSet<T> implements Multiset<T>{
 		this.count += count - oldCount;
 		return oldCount;
 	}
-	@Override
-	public boolean contains(Object element) {
-		return map.containsKey(element);
-	}
-	@Override
-	public boolean add(T element) {
-		map.put(element, map.getOrDefault(element, 0) + 1);
-		count++;
-		return true;
-	}
-	@Override
-	public int add(T element, int occurrences) {
-		if (occurrences < 0) throw new IllegalArgumentException("Occurrences cannot be negative!");
-		int current = map.getOrDefault(element, 0);
-		if (occurrences != 0) {
-			map.put(element, current + occurrences);
-			count += occurrences;
-		}
-		return current;
-	}
-	@SuppressWarnings("unchecked")
-	@Override
-	public boolean remove(Object element) {
-		if (!map.containsKey(element)) return false;
-		int current = map.get(element);
-		if (current > 1) {
-			map.put((T)element, current - 1);
-		} else {
-			map.remove(element);
-		}
-		count--;
-		return true;
-	}
-	@SuppressWarnings("unchecked")
-	@Override
-	public int remove(Object element, int occurrences) {
-		if (occurrences < 0) throw new IllegalArgumentException("Occurrences cannot be negative!");
-		if (!map.containsKey(element)) return 0;
-		int current = map.get(element);
-		if (current > occurrences) {
-			count -= occurrences;
-			map.put((T)element, current - occurrences);
-		} else {
-			count -= current;
-			map.remove(element);
-		}
-		return current;
-	}
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		if (c == null) return false;
-		boolean changed = false;
-		for (Object o : c) {
-			changed = changed || remove(o);
-		}
-		return changed;
-	}
-	@SuppressWarnings("unchecked")
-	@Override
-	public boolean retainAll(Collection<?> c) {
-		if (c == null) return false;
-		boolean changed = false;
-		for (Object o : c) {
-			int count = map.getOrDefault(o, 0);
-			if (count > 0) {
-				map.put((T)o, -count);
-			}
-		}
-		for (Iterator<Map.Entry<T, Integer>> it = map.entrySet().iterator(); it.hasNext();) {
-			Map.Entry<T, Integer> entry = it.next();
-			int value = entry.getValue();
-			if (value > 0) {
-				it.remove();
-				count -= value;
-				changed = true;
-			} else {
-				entry.setValue( - value);
-			}
-		}
-		return changed;
-	}
+	
 	@Override
 	public void clear() {
 		map.clear();
 		count = 0;
 	}
-	@Override
-	public Set<Entry<T>> entrySet() {
-		return new HashMultisetEntrySet();
-	}
+
 	@Override
 	public Set<T> elementSet() {
 		return new HashMultisetElementSet();
-	}
-	@Override
-	public Iterator<T> iterator() {
-		return new HashMultiSetIterator();
-	}
-	
-	
-	private class HashMultiSetIterator implements Iterator<T> {
-
-		private Iterator<Entry<T>> entryIterator;
-		private T current;
-		private T last;
-		private int currentCount = 0;
-		private int i = 0;
-		
-		public HashMultiSetIterator() {
-			entryIterator = entrySet().iterator();
-		}
-		
-		@Override
-		public boolean hasNext() {
-			return i < currentCount || entryIterator.hasNext();
-		}
-
-		@Override
-		public T next() {
-			if (i < currentCount) {
-				i++;
-				return current;
-			}
-			if (entryIterator.hasNext()) {
-				Entry<T> entry = entryIterator.next();
-				current = entry.getElement();
-				last = current;
-				currentCount = entry.getCount();
-				i = 1;
-				return current;
-			}
-			throw new NoSuchElementException();
-		}
-		
-		@Override
-		public void remove() {
-			if (last == null) throw new IllegalStateException();
-			HashMultiset.this.remove(last);
-			last = null;
-		}
-	}
-	
-	private class HashMultisetEntry implements Entry<T> {
-		
-		private Map.Entry<T, Integer> mapEntry;
-		
-		public HashMultisetEntry(Map.Entry<T, Integer> mapEntry) {
-			this.mapEntry = mapEntry;
-		}
-		
-		@Override
-		public T getElement() {return mapEntry.getKey();}
-		@Override
-		public int getCount() {return mapEntry.getValue();}
-		
-	}
-	
-	private class HashMultisetEntrySet extends SetView<Entry<T>> {
-
-		public HashMultisetEntrySet() {
-			super(new SetWrapper<Map.Entry<T, Integer>, Entry<T>>(map.entrySet(), new Function<Map.Entry<T, Integer>, Entry<T>>() {
-				@Override
-				public Entry<T> apply(Map.Entry<T, Integer> t) {return new HashMultisetEntry(t);}
-			}));
-		}
-		
-		@Override
-		protected void addBehavior(Entry<T> element) {
-			throw new UnsupportedOperationException("Cannot add entry to Multiset's entrySet view!");
-		}
-		@Override
-		protected void removeBehavior(Entry<T> element) {
-			count -= element.getCount();
-		}
-		
 	}
 	
 	private class HashMultisetElementSet extends SetView<T> {
@@ -230,7 +61,7 @@ public class HashMultiset<T> extends AbstractSet<T> implements Multiset<T>{
 		
 		@Override
 		protected void addBehavior(T element) {
-			throw new UnsupportedOperationException("Cannot add element to Multiset's elementSet view!");
+			throw new UnsupportedOperationException("Cannot add element to Multiset's view!");
 		}
 		@Override
 		protected void removeBehavior(T element) {
